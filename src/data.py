d9060f83e8cf1641f6768ad7251f3b5dbe0a0566
Emad Ahmed <emad1997@gmail.com>
2024-06-10 17:44:42 -0700

    initial data commit

---
+++ src/data.py
@@ -0,0 +1,93 @@
+import re
+from typing import Dict, List, Tuple
+
+import numpy as np
+import pandas as pandas
+import ray
+from ray.data import Dataset
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer
+
+from src.config import STOPWORDS
+
+def load_data(dataset_loc: str, num_samples: int = None) -> Dataset:
+    # Load data from source into ray dataset
+    ds = ray.data.read_csv(dataset_loc)
+    ds = ds.random_shuffle(seed=1234)
+    ds = ray.data.from_items(ds.take(num_samples)) if num_samples else ds
+
+def stratify_split(
+    ds: Dataset,
+    stratify: str,
+    test_size: float,
+    shuffle: bool = True,
+    seed: int = 1234,
+) -> Tuple[Dataset, Dataset]:
+    """Split a dataset into train and test splits with equal
+    amounts of data points from each class in the column we
+    want to stratify on.
+
+    Args:
+        ds (Dataset): Input dataset to split.
+        stratify (str): Name of column to split on.
+        test_size (float): Proportion of dataset to split for test set.
+        shuffle (bool, optional): whether to shuffle the dataset. Defaults to True.
+        seed (int, optional): seed for shuffling. Defaults to 1234.
+
+    Returns:
+        Tuple[Dataset, Dataset]: the stratified train and test datasets.
+    """
+
+    def _add_split(df: pd.DataFrame) -> pd.DataFrame:  # pragma: no cover, used in parent function
+        """Naively split a dataframe into train and test splits.
+        Add a column specifying whether it's the train or test split."""
+        train, test = train_test_split(df, test_size=test_size, shuffle=shuffle, random_state=seed)
+        train["_split"] = "train"
+        test["_split"] = "test"
+        return pd.concat([train, test])
+
+    def _filter_split(df: pd.DataFrame, split: str) -> pd.DataFrame:  # pragma: no cover, used in parent function
+        """Filter by data points that match the split column's value
+        and return the dataframe with the _split column dropped."""
+        return df[df["_split"] == split].drop("_split", axis=1)
+
+    # Train, test split with stratify
+    grouped = ds.groupby(stratify).map_groups(_add_split, batch_format="pandas")  # group by each unique value in the column we want to stratify on
+    train_ds = grouped.map_batches(_filter_split, fn_kwargs={"split": "train"}, batch_format="pandas")  # combine
+    test_ds = grouped.map_batches(_filter_split, fn_kwargs={"split": "test"}, batch_format="pandas")  # combine
+
+    # Shuffle each split (required)
+    train_ds = train_ds.random_shuffle(seed=seed)
+    test_ds = test_ds.random_shuffle(seed=seed)
+
+    return train_ds, test_ds
+
+def tokenize(batch: Dict) -> Dict:
+    tokenizer = BertTokenizer.from_pretrained("allenai/scibert_scivocab_uncased", return_dict=FalseS)
+
+
+def clean_text(text: str, stopwords: List = STOPWORDS) -> str:
+    """Clean raw text string.
+
+    Args:
+        text (str): Raw text to clean.
+        stopwords (List, optional): list of words to filter out. Defaults to STOPWORDS.
+
+    Returns:
+        str: cleaned text.
+    """
+    # Lower
+    text = text.lower()
+
+    # Remove stopwords
+    pattern = re.compile(r"\b(" + r"|".join(stopwords) + r")\b\s*")
+    text = pattern.sub(" ", text)
+
+    # Spacing and filters
+    text = re.sub(r"([!\"'#$%&()*\+,-./:;<=>?@\\\[\]^_`{|}~])", r" \1 ", text)  # add spacing
+    text = re.sub("[^A-Za-z0-9]+", " ", text)  # remove non alphanumeric chars
+    text = re.sub(" +", " ", text)  # remove multiple spaces
+    text = text.strip()  # strip white space at the ends
+    text = re.sub(r"http\S+", "", text)  # remove links
+
+    return text
